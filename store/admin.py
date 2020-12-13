4693062d20e4a2cf606aa3561bff524666382436
Aravind Hari <avdhari114@gmail.com>
2020-12-13 17:23:24 +0530

    added static and updated model.py v1.0

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Food
+from .models import *
 
-admin.site.register(Food)
+admin.site.register(Food)   
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- store/models.py
+++ store/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 
 class Food(models.Model):
     id = models.AutoField(primary_key=True)
@@ -11,3 +20,25 @@ class Food(models.Model):
 
     def __str__(self):
         return self.name
+
+        
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_order = models.DateField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=True)
+    transaction_id = models.CharField(max_length=50, null=True)
+
+
+    #  def __str__(self):
+    #     return self.Food.name
+
+
+
+class OrderItem(models.Model):
+    food = models.ForeignKey(Food, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+#class for UDT generation.  
