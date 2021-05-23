5d7f5a7bec3cdc31020ac88a7f895db5e6710e47
avdhari <avdhari114@gmail.com>
2021-05-23 16:18:07 +0530

    aavo

--- store/models.py
+++ store/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
+    #user = models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
 
